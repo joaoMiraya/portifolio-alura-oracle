0e0be131032eb6cb837e45cdc63ad5eb7cf88c47
Joao Miraya <joaomiraya01@gmail.com>
2023-06-25 11:14:09 -0300

    att

--- portifaOracle/src/App.jsx
+++ portifaOracle/src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css';
+import { useRef } from 'react';
+
 import Header from './components/patterns/Header';
 import Home from './components/pages/Home';
 import SobreMim from './components/pages/SobreMim';
@@ -11,18 +13,63 @@ import Footer from './components/patterns/Footer';
 
 
 function App() {
+  const sobreRef = useRef();
+  const skillsRef = useRef();
+  const hobbiesRef = useRef();
+  const formacaoRef = useRef();
+  const projetosRef = useRef();
 
 
-
+  function handleBackToAbout() {
+    const aboutPosition = sobreRef.current.offsetTop;
+    window.scrollTo({ top: aboutPosition, behavior: 'smooth' });
+  }
+  function handleBackToSkills() {
+    const skillsPosition = skillsRef.current.offsetTop;
+    window.scrollTo({ top: skillsPosition, behavior: 'smooth' });
+  }
+  function handleBackToHobbies() {
+    const hobbiesPosition = hobbiesRef.current.offsetTop;
+    window.scrollTo({ top: hobbiesPosition, behavior: 'smooth' });
+  }
+  function handleBackToFormation() {
+    const formationPosition = formacaoRef.current.offsetTop;
+    window.scrollTo({ top: formationPosition, behavior: 'smooth' });
+  }
+  function handleBackToProjects() {
+    const projectPosition = projetosRef.current.offsetTop;
+    window.scrollTo({ top: projectPosition, behavior: 'smooth' });
+  }
   return (
     <>
-      <Header />
+      <Header
+        handleBackToAbout={handleBackToAbout}
+        handleBackToSkills={handleBackToSkills}
+        handleBackToHobbies={handleBackToHobbies}
+        handleBackToFormation={handleBackToFormation}
+        handleBackToProjects={handleBackToProjects}
+      />
       <Home />
-      <SobreMim />
-      <Skills />
-      <Hobbies />
-      <Formacao />
-      <Experiencia />
+      <div ref={sobreRef}>
+        <SobreMim />
+      </div>
+
+      <div ref={skillsRef}>
+        <Skills />
+      </div>
+
+      <div ref={hobbiesRef}>
+        <Hobbies />
+      </div>
+
+      <div ref={formacaoRef}>
+        <Formacao />
+      </div>
+
+      <div ref={projetosRef}>
+        <Experiencia />
+      </div>
+
       <Contato />
       <Footer />
     </>
--- portifaOracle/src/components/patterns/Header.jsx
+++ portifaOracle/src/components/patterns/Header.jsx
@@ -1,15 +1,15 @@
 
-function Header() {
+function Header({ handleBackToProjects, handleBackToAbout, handleBackToSkills, handleBackToHobbies, handleBackToFormation }) {
     return (
         <nav className="p-2">
             <div className="containerr flex justify-between items-center h-20">
                 <a href="/">  <h1 className=" text-xl md:text-2xl xl:text-3xl ">João Miraya</h1></a>
                 <ul className=" flex gap-4">
-                    <li className="nav-links hover:underline cursor-pointer hidden md:flex">Sobre Mim</li>
-                    <li className="nav-links hover:underline cursor-pointer hidden md:flex">Skills</li>
-                    <li className="nav-links hover:underline cursor-pointer hidden md:flex">Hobbies</li>
-                    <li className="nav-links hover:underline cursor-pointer hidden md:flex">Formação</li>
-                    <li className="nav-links hover:underline cursor-pointer hidden md:flex">Projetos</li>
+                    <li onClick={handleBackToAbout} className="nav-links hover:underline cursor-pointer hidden md:flex">Sobre Mim</li>
+                    <li onClick={handleBackToSkills} className="nav-links hover:underline cursor-pointer hidden md:flex">Skills</li>
+                    <li onClick={handleBackToHobbies} className="nav-links hover:underline cursor-pointer hidden md:flex">Hobbies</li>
+                    <li onClick={handleBackToFormation} className="nav-links hover:underline cursor-pointer hidden md:flex">Formação</li>
+                    <li onClick={handleBackToProjects} className="nav-links hover:underline cursor-pointer hidden md:flex">Projetos</li>
                     <a href="mailto:joaomiraya01@gmail.com?subject=Assunto do Email"><p className="font-bold text-sm md:text-xl ">joaomiraya01@gmail.com</p></a>
                 </ul>
             </div>
