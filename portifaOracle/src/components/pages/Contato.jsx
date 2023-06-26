db157a0bcb4470c76ab50fad4c91b71dc8a13d36
Joao Miraya <joaomiraya01@gmail.com>
2023-06-26 09:32:52 -0300

    corrigindo quebra do form

--- portifaOracle/src/components/pages/Contato.jsx
+++ portifaOracle/src/components/pages/Contato.jsx
@@ -94,7 +94,7 @@ function Contato() {
                         <p className=' text-sm'>Quer entrar em contato comigo? <br />
                             Preencha o formulário ao lado e entrarei em contato o mais rápido possível.</p>
                     </div>
-                    <div className='flex flex-col'>
+                    <div className='flex flex-col px-2 md:w-full '>
                         <label ref={refNameLabel} className='text-[10px] relative top-4 left-2 z-10 text-gray-400' htmlFor="nome">Nome</label>
                         <input
                             ref={refName}
